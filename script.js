3a9352c6ca160d1f6004857f5d25d4399e485486
David Freitas <davidcnt@outlook.com>
2023-06-28 19:42:42 -0300

    Fazendo testes com JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+function soma(x, y) {
+    console.log(x + y)
+}
+
+soma(3, 4)
+soma(4, 6)
+soma(4, 16)
+
+let pessoa = {
+    nome: "David",
+    idade: 22,
+    funcao: "Programador"
+}
+
+console.log(pessoa.nome)
+soma(pessoa.idade, 5)
